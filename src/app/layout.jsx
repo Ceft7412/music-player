e85381b984380f82b6edf606b9c2c210786d2bf6
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-13 16:54:01 +0800

    added: root provider to pass data to children

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
 import "../resources/sass/styles.scss";
+import { RootProvider } from "@/context/RootContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Linger",
-  description: "Linger is an app that lets you listen to music and read lyrics at the same time.",
+  description:
+    "Linger is an app that lets you listen to music and read lyrics at the same time.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <RootProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </RootProvider>
   );
 }
