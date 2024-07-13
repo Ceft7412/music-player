5a2e51aabddf80c1d5333bcc832f151cc654c05f
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-13 08:18:29 +0800

    added: layout for the music player

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,24 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="">What</div>
+      <div className="music">
+        {/*  */}
+        {/* column */}
+        <div className="music__layout">
+          {/*  */}
+          {/* row */}
+          <div className="">
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+          </div>
+          {/*  */}
+          {/* row */} 
+          <div className="">
+            <div className=""></div>
+          </div>
+        </div> 
+      </div>
     </>
   );
 }
