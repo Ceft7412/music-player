fc203f6e86a401ac1e3d2ca7de68afa90383868f
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-13 15:41:51 +0800

    created: final layout for the music player

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import Footer from "@/components/footer";
 export default function Home() {
   return (
@@ -11,9 +12,25 @@ export default function Home() {
           {/* row */}
           <div className="row content">
             <div className="row__flex_row">
-              <div className=""></div>
-              <div className=""></div>
-              <div className=""></div>
+              <div className="row__flex_row_item row__flex_row_left"></div>
+              <div className="row__flex_row_item row__flex_row_center"></div>
+              <div className="row__flex_row_item row__flex_row_right">
+                <div className="music">
+                  <div className="music__flex">
+                    <div className="music__header">
+                      <div className="music__header_item music__playlist">
+                        <span className="active">Default</span>
+                        <span>Default</span>
+                        <span>Default</span>
+                      </div>
+                      <div className="music__header_item ">
+                        <AddRoundedIcon />
+                      </div>
+                    </div>
+                    <div className="music__body"></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
