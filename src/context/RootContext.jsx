cc944b03d13026833cecee341c8bc952353dc7de
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-15 06:03:19 +0800

    Update RootContext.jsx

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -4,6 +4,7 @@ export const RootContext = createContext();
 export function RootProvider({ children }) {
   const [modalUpload, setModalUpload] = useState(false);
   const [storedFiles, setStoredFiles] = useState([]);
+  const [itemToPlay, setItemToPlay] = useState(null);
   const [backgroundClickedItem, setBackgroundClickedItem] = useState("");
   return (
     <RootContext.Provider
@@ -11,6 +12,8 @@ export function RootProvider({ children }) {
         modalUpload,
         setModalUpload,
         storedFiles,
+        itemToPlay,
+        setItemToPlay,
         setStoredFiles,
         backgroundClickedItem,
         setBackgroundClickedItem,
