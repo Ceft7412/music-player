4ad9a5f27275083d84d1b0a7ef4e6d04d7698ca4
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-14 12:54:11 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-
+import PlayCircleFilledRoundedIcon from "@mui/icons-material/PlayCircleFilledRounded";
+import SkipNextRoundedIcon from "@mui/icons-material/SkipNextRounded";
+import SkipPreviousRoundedIcon from "@mui/icons-material/SkipPreviousRounded";
 export default function Footer() {
   return (
     <>
       <div className="footer">
-        <div className="footer__flex"></div>
+        <div className="footer__flex">
+          <div className="footer__flex-cols">
+            <div className="">
+              <SkipPreviousRoundedIcon style={{ fontSize: 40 }} />
+              <PlayCircleFilledRoundedIcon style={{ fontSize: 40 }} />
+              <SkipNextRoundedIcon style={{ fontSize: 40 }} />
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
       </div>
     </>
   );
