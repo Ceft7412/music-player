9bd1b0c5192ab39cbd9112822205aec7e98f8d7b
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-14 12:43:58 +0800

    Update landing.jsx

--- src/app/landing.jsx
+++ src/app/landing.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react";
-import AddRoundedIcon from "@mui/icons-material/AddRounded";
-import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+
 import Footer from "@/components/footer";
 import AlbumRoundedIcon from "@mui/icons-material/AlbumRounded";
 import ModalUpload from "@/components/ModalUpload";
-import { RootContext } from "@/context/RootContext";
+  import { RootContext } from "@/context/RootContext";
+import Music from "@/components/Music";
 
 export default function Landing() {
-  const { setModalUpload, modalUpload, storedFiles } = useContext(RootContext);
-  console.log("storedFiles:", storedFiles);
+  const { setModalUpload, modalUpload } = useContext(RootContext);
   const handleClose = () => {
     if (modalUpload) {
       setModalUpload(false);
@@ -35,62 +34,7 @@ export default function Landing() {
               <div className="row__flex_row_item row__flex_row_center">
                 <div className="row__flex_row_center-flex"></div>
               </div>
-              <div className="row__flex_row_item row__flex_row_right">
-                <div className="music-what">
-                  <div className="music__flex">
-                    <div className="music__header">
-                      <div className="music__header_item music__playlist">
-                        <span className="active">Default</span>
-                        <span>Default</span>
-                        <span>Default</span>
-                      </div>
-                      <div
-                        className="music__header_item music__upload"
-                        onClick={() => setModalUpload(true)}
-                      >
-                        <AddRoundedIcon />
-                      </div>
-                    </div>
-                    <div className="music__body">
-                      <div className="music__body_flex">
-                        <div className="bodyhead">
-                          <input type="checkbox" />
-                          <div className="bodyhead__icon">
-                            <KeyboardArrowDownRoundedIcon />
-                          </div>
-                        </div>
-                        <div className="bodycontent">
-                          <div className="bodycontent__flex">
-                            {storedFiles.map((file, index) => (
-                              <div className="bodycontent__flex_item" key={index}>
-                                <div className="music__item">
-                                  <input type="checkbox" />
-                                  <div className="col__inside">
-                                    <span>
-                                      {index + 1}. {file.artist} - {file.name}
-                                    </span>
-                                    <span className="info__inside">
-                                      {file.type}, {file.kHz} kHz, {file.kbps} kbps,{" "}
-                                      {file.size}
-                                    </span>
-                                  </div>
-                                </div>
-                                <div className="">
-                                  <span>
-                                    {file.duration.minutes}:
-                                    {file.duration.seconds < 10 ? "0" : ""}
-                                    {file.duration.seconds}
-                                  </span>
-                                </div>
-                              </div>
-                            ))}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Music />
             </div>
           </div>
         </div>
