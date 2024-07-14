1cba17d0652236e9a6e0a0423c63f17a1ed480a8
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-14 12:54:08 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && isServer) {
+      config.externals = {
+        ...config.externals,
+        react: "commonjs react",
+        "react-dom": "commonjs react-dom",
+      };
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
