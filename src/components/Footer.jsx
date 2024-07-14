d07237da51097dc81613f8cfc499e43241d4b860
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-14 11:55:38 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <div className="footer">
-        <div className=""></div>
+        <div className="footer__flex"></div>
       </div>
     </>
   );
