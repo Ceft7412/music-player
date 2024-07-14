394d7dbab7691bb98e413c9f0a6cdb84c6afd3f0
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-14 13:12:37 +0800

    Update RootContext.jsx

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -4,9 +4,17 @@ export const RootContext = createContext();
 export function RootProvider({ children }) {
   const [modalUpload, setModalUpload] = useState(false);
   const [storedFiles, setStoredFiles] = useState([]);
+  const [backgroundClickedItem, setBackgroundClickedItem] = useState("");
   return (
     <RootContext.Provider
-      value={{ modalUpload, setModalUpload, storedFiles, setStoredFiles }}
+      value={{
+        modalUpload,
+        setModalUpload,
+        storedFiles,
+        setStoredFiles,
+        backgroundClickedItem,
+        setBackgroundClickedItem,
+      }}
     >
       {children}
     </RootContext.Provider>
