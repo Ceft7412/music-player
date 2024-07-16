edf14240223b56931591aaff8af7fe8e80c107d3
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-16 21:31:25 +0800

    Update ModalPlaylist.jsx

--- src/components/ModalPlaylist.jsx
+++ src/components/ModalPlaylist.jsx
@@ -1,55 +1,30 @@
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import RemoveCircleOutlineOutlinedIcon from "@mui/icons-material/RemoveCircleOutlineOutlined";
+import { RootContext } from "@/context/RootContext";
+import { useContext } from "react";
 export default function ModalPlaylist({ x, y, onClose, playlist }) {
+  const { setModalEditPlaylist } = useContext(RootContext);
   return (
     <>
       <div
-        className="modal"
+        className="modal-playlist"
         style={{
-          position: "absolute",
-          display: "flex",
-          flexDirection: "column",
-
           left: x,
           top: y,
-          backgroundColor: "rgb(27, 27, 34)",
-          borderRight: "2px solid orange",
-          borderBottom: "2px solid orange",
-
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
-          zIndex: 999,
         }}
       >
-        <div
-          className="modal-playlist"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "10px",
-            cursor: "pointer",
-            padding: "10px",
-          }}
-        >
+        <div className="modal-playlist__item" onClick={() => setModalEditPlaylist(true)}>
           <span>
             <ModeEditOutlineOutlinedIcon style={{ fontSize: "20px" }} />
           </span>
-          <span style={{ fontSize: "14px" }}>Edit details</span>
+          <span className="modal-playlist__item-t">Edit details</span>
         </div>
 
-        <div
-          className="modal-playlist"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "10px",
-            cursor: "pointer",
-            padding: "10px",
-          }}
-        >
+        <div className="modal-playlist__item">
           <span>
             <RemoveCircleOutlineOutlinedIcon style={{ fontSize: "20px" }} />
           </span>
-          <span style={{ fontSize: "14px" }}>Delete playlist</span>
+          <span className="modal-playlist__item-t">Delete playlist</span>
         </div>
       </div>
     </>
