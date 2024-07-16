60f7e3e5a8839c588cc8b42f3c091814c230bd09
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-16 18:13:21 +0800

    Update RootContext.jsx

--- src/context/RootContext.jsx
+++ src/context/RootContext.jsx
@@ -1,20 +1,41 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 export const RootContext = createContext();
 
 export function RootProvider({ children }) {
   const [modalUpload, setModalUpload] = useState(false);
+  const [modalPlaylist, setModalPlaylist] = useState(false);
   const [storedFiles, setStoredFiles] = useState([]);
   const [itemToPlay, setItemToPlay] = useState(null);
   const [backgroundClickedItem, setBackgroundClickedItem] = useState("");
+
+  const [playlists, setPlaylists] = useState(null);
+
+  console.log(playlists);
+
+  const [volume, setVolume] = useState(() => {
+    const volumeRange = localStorage.getItem("volume");
+    return Number(volumeRange) || 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("volume", volume);
+  }, [volume]);
+
   return (
     <RootContext.Provider
       value={{
         modalUpload,
         setModalUpload,
+        modalPlaylist,
+        setModalPlaylist,
         storedFiles,
         itemToPlay,
         setItemToPlay,
         setStoredFiles,
+        setPlaylists,
+        playlists,
+        volume,
+        setVolume,
         backgroundClickedItem,
         setBackgroundClickedItem,
       }}
