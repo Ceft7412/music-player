fa1c79cdece198810e8e24934c8f4ff8b1ba6c90
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-17 15:13:56 +0800

    Update ModalEdit.jsx

--- src/components/ModalEdit.jsx
+++ src/components/ModalEdit.jsx
@@ -5,6 +5,9 @@ import MusicNoteOutlinedIcon from "@mui/icons-material/MusicNoteOutlined";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
 import { updatePlaylist, fetchPlaylists } from "@/utils/indexdb";
+
+import ImageRoundedIcon from "@mui/icons-material/ImageRounded";
+import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
 export default function ModalEdit() {
   const {
     modalEditPlaylist,
@@ -12,15 +15,37 @@ export default function ModalEdit() {
     playlists,
     setPlaylists,
     playlistIndex,
+    currentPlaylist,
+    setCurrentPlaylist,
+    photoModal,
+    setPhotoModal,
   } = useContext(RootContext);
-  const [currentPlaylist, setCurrentPlaylist] = useState(null);
 
   const [showDiffIcon, setShowDiffIcon] = useState(false);
+  const [image, setImage] = useState(null);
   const fileInput = useRef(null);
-  const cardRef = useRef(null); // Create a ref
+  const cardRef = useRef(null);
+  const photoModalRef = useRef();
+  const moreSetPhotoModal = useRef();
 
   useEffect(() => {
-    if (playlists && playlistIndex) {
+    function handleClickOutside(event) {
+      if (photoModalRef.current && !photoModalRef.current.contains(event.target)) {
+        // Clicked outside of the modal, close it
+        setPhotoModal(false);
+      }
+    }
+
+    // Attach the listeners on component mount.
+    document.addEventListener("mousedown", handleClickOutside);
+    // Detach the listeners on component unmount.
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [photoModalRef]);
+
+  useEffect(() => {
+    if (playlists && playlistIndex !== null && playlistIndex !== undefined) {
       setCurrentPlaylist(playlists[playlistIndex]);
     }
   }, [playlists, playlistIndex]);
@@ -35,14 +60,36 @@ export default function ModalEdit() {
     fileInput.current.click();
   };
 
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setCurrentPlaylist({ ...currentPlaylist, image: reader.result });
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleNameChange = (event) => {
     setCurrentPlaylist({ ...currentPlaylist, name: event.target.value });
+  };
+
+  const handleDescriptionChange = (event) => {
+    setCurrentPlaylist({ ...currentPlaylist, description: event.target.value });
   };
 
   const handleSubmit = async () => {
     if (currentPlaylist) {
       try {
-        await updatePlaylist(currentPlaylist.id, currentPlaylist.name);
+        await updatePlaylist(
+          currentPlaylist.id,
+          currentPlaylist.name,
+          currentPlaylist.description,
+          currentPlaylist.image
+        );
         const updatedPlaylists = await fetchPlaylists();
         setPlaylists(updatedPlaylists);
         setModalEditPlaylist(false);
@@ -67,40 +114,71 @@ export default function ModalEdit() {
               </div>
             </div>
             <div className="modal-edit-playlist__card-body">
+              {photoModal && (
+                <div className="modal-photo" ref={photoModalRef}>
+                  <div className="modal-photo__item" onClick={handleInputClick}>
+                    <div className="modal-photo__icon">
+                      <ImageRoundedIcon style={{ color: "rgb(201, 201, 201)" }} />
+                    </div>
+                    <span className="modal-photo__text">Change photo</span>
+                  </div>
+                  <div className="modal-photo__item">
+                    <div className="modal-photo__icon">
+                      <DeleteOutlineRoundedIcon style={{ color: "rgb(201, 201, 201)" }} />
+                    </div>
+                    <span className="modal-photo__text">Remove photo</span>
+                  </div>
+                </div>
+              )}
+
               <div
                 className="modal-edit-playlist__card-body-item image-edit"
                 onMouseEnter={() => setShowDiffIcon(true)}
                 onMouseLeave={() => setShowDiffIcon(false)}
               >
-                <div style={{ position: "relative" }}>
+                <div
+                  className="modal-edit-playlist__image-icons-container"
+                  style={{ position: "relative" }}
+                >
                   {showDiffIcon && (
-                    <div style={{ position: "absolute", right: "-20px", top: "-20px" }}>
+                    <div
+                      className="modal-edit-playlist__more-icon-container"
+                      style={{ position: "absolute", right: "10px", top: "10px" }}
+                      onClick={() => {
+                        setPhotoModal(!photoModal);
+                      }}
+                    >
                       <MoreHorizOutlinedIcon />
                     </div>
                   )}
-
-                  {showDiffIcon ? (
+                  {currentPlaylist.image ? (
+                    <img
+                      className="modal-edit-playlist__image"
+                      src={currentPlaylist.image}
+                      alt={currentPlaylist.name}
+                    />
+                  ) : (
                     <div
                       style={{
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "center",
                       }}
-                      onClick={handleInputClick}
                     >
-                      <ModeEditOutlineOutlinedIcon
-                        style={{ fontSize: "100px", color: "white" }}
-                      />
-                      <span>Choose a photo</span>
+                      <>
+                        <MusicNoteOutlinedIcon
+                          style={{ fontSize: "100px", color: "gray" }}
+                        />
+                        <span style={{ color: "gray" }}>Choose a photo</span>
+                      </>
                     </div>
-                  ) : (
-                    <MusicNoteOutlinedIcon style={{ fontSize: "100px", color: "gray" }} />
                   )}
                   <input
                     type="file"
+                    accept="image/*"
                     ref={fileInput}
                     style={{ display: "none" }}
-                    // onChange={handleFileChange}
+                    onChange={handleFileChange}
                   />
                 </div>
               </div>
@@ -115,7 +193,12 @@ export default function ModalEdit() {
                 </div>
                 <div className="cols-edit__description">
                   <label>Description</label>
-                  <textarea type="text" placeholder="Add description" />
+                  <textarea
+                    type="text"
+                    placeholder="Add description"
+                    value={currentPlaylist.description || ""}
+                    onChange={handleDescriptionChange}
+                  />
                 </div>
               </div>
             </div>
